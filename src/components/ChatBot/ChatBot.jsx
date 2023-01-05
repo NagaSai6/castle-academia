dad0c2b47d27beb9a4b75d3c8beb322a902e8e91
naga7 <naga@wishup.co>
2023-01-05 18:42:43 +0530

    #chatbot_form_integration

--- src/components/ChatBot/ChatBot.jsx
+++ src/components/ChatBot/ChatBot.jsx
@@ -1,30 +1,55 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import ChatBot from 'react-simple-chatbot';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import ChatBot from "react-simple-chatbot";
+import axios from "axios";
+
+function SendMail(props) {
+  const { steps } = props;
+  const { name, courses, phoneNumber, email } = steps;
+  const data = {
+    name: name.value,
+    courses: courses.value,
+    phoneNumber: phoneNumber.value,
+    email: email.value,
+  };
+  const configuration = {
+    method: "post",
+    url: "https://castle-academia-server.onrender.com/send-mail-chatbot",
+    data: data,
+  };
+  axios(configuration)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  return "";
+}
 
 class Review extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      name: '',
-      courses : '',
-      phoneNumber : '',
-      email : ''
+      name: "",
+      courses: "",
+      phoneNumber: "",
+      email: "",
     };
   }
 
   componentWillMount() {
     const { steps } = this.props;
-    const { name, courses, phoneNumber,email} = steps;
+    const { name, courses, phoneNumber, email } = steps;
 
-    this.setState({ name, courses, phoneNumber,email });
+    this.setState({ name, courses, phoneNumber, email });
   }
 
   render() {
-    const { name, courses, phoneNumber,email} = this.state;
+    const { name, courses, phoneNumber, email } = this.state;
     return (
-      <div style={{ width: '100%' }}>
+      <div style={{ width: "100%" }}>
         <h3>Summary</h3>
         <table>
           <tbody>
@@ -51,6 +76,13 @@ class Review extends Component {
   }
 }
 
+SendMail.prototypes = {
+  steps: PropTypes.object,
+};
+SendMail.defaultProps = {
+  steps: undefined,
+};
+
 Review.propTypes = {
   steps: PropTypes.object,
 };
@@ -63,49 +95,53 @@ class ChatBotForm extends Component {
   render() {
     return (
       <ChatBot
-      width="300px"
-      bubbleStyle= {{backgroundColor : "#FDD03B"}}
-      floatingStyle={{backgroundColor : "#000"}}
-      headerTitle="Castle Academia"
-        floating = {true}
+        width="300px"
+        bubbleStyle={{ backgroundColor: "#FDD03B" }}
+        floatingStyle={{ backgroundColor: "#000" }}
+        headerTitle="Castle Academia"
+        floating={true}
         steps={[
           {
-            id: '1',
-            message: 'What is your name?',
-            trigger: 'name',
+            id: "1",
+            message: "What is your name?",
+            trigger: "name",
           },
           {
-            id: 'name',
+            id: "name",
             user: true,
-            trigger: '3',
+            trigger: "3",
           },
           {
-            id: '3',
-            message: 'Hi {previousValue}! What is your Interested Course?',
-            trigger: 'courses',
+            id: "3",
+            message: "Hi {previousValue}! What is your Interested Course?",
+            trigger: "courses",
           },
           {
-            id: 'courses',
+            id: "courses",
             options: [
-              { value: 'CRT Training', label: 'CRT Training', trigger: '5' },
-              { value: 'Start Up Plan', label: 'Start-Up Plan', trigger: '5' },
-              { value: 'Abroad Studies', label: 'Abroad Studies', trigger: '5' },
+              { value: "CRT Training", label: "CRT Training", trigger: "5" },
+              { value: "Start Up Plan", label: "Start-Up Plan", trigger: "5" },
+              {
+                value: "Abroad Studies",
+                label: "Abroad Studies",
+                trigger: "5",
+              },
             ],
           },
           {
-            id: '5',
-            message: 'Enter your phone number ',
-            trigger: 'phoneNumber',
+            id: "5",
+            message: "Enter your phone number ",
+            trigger: "phoneNumber",
           },
           {
-            id: 'phoneNumber',
+            id: "phoneNumber",
             user: true,
-            trigger: '100',
+            trigger: "100",
             validator: (value) => {
               if (!value.length) {
-                return 'Enter Valid Number';
+                return "Enter Valid Number";
               } else if (value.length < 6) {
-                return 'Enter Valid Phone Number';
+                return "Enter Valid Phone Number";
               } else if (value.length > 120) {
                 return `${value}? Come on!`;
               }
@@ -114,77 +150,86 @@ class ChatBotForm extends Component {
             },
           },
           {
-            id  : '100',
-            message : 'Enter your Email Address',
-            trigger : 'email'
+            id: "100",
+            message: "Enter your Email Address",
+            trigger: "email",
           },
           {
-            id : 'email',
-            user : true,
-            trigger : '7'
-
-         },
+            id: "email",
+            user: true,
+            trigger: "7",
+          },
           {
-            id: '7',
-            message: 'Great! Check out your summary',
-            trigger: 'review',
+            id: "7",
+            message: "Great! Check out your summary",
+            trigger: "review",
           },
 
           {
-            id: 'review',
+            id: "review",
             component: <Review />,
             asMessage: true,
-            trigger: 'update',
+            trigger: "update",
           },
           {
-            id: 'update',
-            message: 'Would you like to update some field?',
-            trigger: 'update-question',
+            id: "update",
+            message: "Would you like to update some field?",
+            trigger: "update-question",
           },
           {
-            id: 'update-question',
+            id: "update-question",
             options: [
-              { value: 'yes', label: 'Yes', trigger: 'update-yes' },
-              { value: 'no', label: 'No', trigger: 'end-message' },
+              { value: "yes", label: "Yes", trigger: "update-yes" },
+              { value: "no", label: "No", trigger: "send-data" },
             ],
           },
           {
-            id: 'update-yes',
-            message: 'What field would you like to update?',
-            trigger: 'update-fields',
+            id: "send-data",
+            component: <SendMail />,
+            asMessage: true,
+            trigger: "end-message",
           },
           {
-            id: 'update-fields',
+            id: "update-yes",
+            message: "What field would you like to update?",
+            trigger: "update-fields",
+          },
+          {
+            id: "update-fields",
             options: [
-              { value: 'name', label: 'Name', trigger: 'update-name' },
-              { value: 'courses', label: 'Courses', trigger: 'update-courses' },
-              { value: 'phoneNumber', label: 'Phone Number', trigger: 'update-phoneNumber' },
-              { value: 'email', label: 'Email', trigger: 'update-email' },
+              { value: "name", label: "Name", trigger: "update-name" },
+              { value: "courses", label: "Courses", trigger: "update-courses" },
+              {
+                value: "phoneNumber",
+                label: "Phone Number",
+                trigger: "update-phoneNumber",
+              },
+              { value: "email", label: "Email", trigger: "update-email" },
             ],
           },
           {
-            id: 'update-name',
-            update: 'name',
-            trigger: '7',
+            id: "update-name",
+            update: "name",
+            trigger: "7",
           },
           {
-            id: 'update-courses',
-            update: 'courses',
-            trigger: '7',
+            id: "update-courses",
+            update: "courses",
+            trigger: "7",
           },
           {
-            id: 'update-phoneNumber',
-            update: 'phoneNumber',
-            trigger: '7',
+            id: "update-phoneNumber",
+            update: "phoneNumber",
+            trigger: "7",
           },
           {
-            id: 'update-email',
-            update: 'email',
-            trigger: '7',
+            id: "update-email",
+            update: "email",
+            trigger: "7",
           },
           {
-            id: 'end-message',
-            message: 'Thanks! Your data was submitted successfully!',
+            id: "end-message",
+            message: "Thanks! Your data was submitted successfully!",
             end: true,
           },
         ]}
